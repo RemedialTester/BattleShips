ac007cfcd7061181af9263169c4430529bbaa8be
Aretheus <bluechaos0199@gmail.com>
2015-10-11 16:29:27 +1100

    added easy ai option, needs testing

--- src/GameController.cs
+++ src/GameController.cs
@@ -96,8 +96,11 @@ namespace BattleShips
 				case AIOption.Hard:
 					_ai = new AIHardPlayer(_theGame);
 					break;
+				case AIOption.Easy:
+					_ai = new AIEasyPlayer (_theGame);
+					break;
 				default:
-					_ai = new AIHardPlayer(_theGame);
+					_ai = new AIMediumPlayer(_theGame);
 					break;
 			}
 
